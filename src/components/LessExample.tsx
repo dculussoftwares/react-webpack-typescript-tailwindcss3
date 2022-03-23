ea424b273111543ec852173c505fbe547c2f00af
natheeshkumar.r <natheeshkumar.r@thoughtworks.com>
2022-03-23 07:02:23 +0530

    Add less sample

--- src/components/LessExample.tsx
+++ src/components/LessExample.tsx
@@ -5,6 +5,32 @@ const LessExample = () => {
   return (
     <div>
       <button className="red-button">Less example</button>
+
+      <h2>Easy Less buttons (work in progress)</h2>
+      <a className="btn-roxo" href="#a">
+        Button
+      </a>
+      <a className="btn-red" href="#a">
+        Button
+      </a>
+      <a className="btn-black" href="#a">
+        Button
+      </a>
+      <a className="btn-something" href="#a">
+        Button
+      </a>
+      <a className="btn-light" href="#a">
+        Button
+      </a>
+      <a className="btn-green" href="#a">
+        Button
+      </a>
+      <a className="thiago" href="#a">
+        Thiago
+      </a>
+      <a className="blue" href="#a">
+        aceitar
+      </a>
     </div>
   );
 };
